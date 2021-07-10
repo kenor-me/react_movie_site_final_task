8f4969ac5bc2392cfc43db7faf2d65271c61fbea
Katherine Kanareikina <katyabrizitskaya@mail.ru>
2021-07-10 19:35:07 +0300

    init: start movie site task

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:jsx-a11y/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
